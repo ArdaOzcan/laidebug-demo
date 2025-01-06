56c47a0ff929586804c148d5331a7d1dbbb79e1a
itu-itis24-ozcanar23 <ozcanar23@itu.edu.tr>
2025-01-06 19:14:30 +0300

    Changed example client to read from file.

--- example_client/client.js
+++ example_client/client.js
@@ -1,24 +1,14 @@
+// Your API key goes here
 const API_KEY = "";
 
-const fileContent = `
-def add(x, y):
-    sum = 0
-    for n in [x, y]:
-        sum += n
-
-    return sum
-
-
-if __name__ == "__main__":
-    add(35, 40)
-`;
-
+const fs = require('fs');
+const fileContent = fs.readFileSync('./example.py', 'utf8');
 
 fetch("http://localhost:5000", {
   method: "POST",
   body: JSON.stringify({
     method: "debugFunction",
-    params: [fileContent, "adr", "gemini-1.5-flash", API_KEY],
+    params: [fileContent, "add", "gemini-1.5-flash", API_KEY],
     id: 1,
   }),
   headers: {
---
+++ example_client/example.py
@@ -0,0 +1,10 @@
+def add(x, y):
+    sum = 0
+    for n in [x, y]:
+        sum += n
+
+    return sum
+
+
+if __name__ == "__main__":
+    add(35, 40)
